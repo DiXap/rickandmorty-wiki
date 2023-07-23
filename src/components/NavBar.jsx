981d50984810be0ee9227591bff409c49f309145
Diego Padilla <dpadlara@ciencias.unam.mx>
2023-07-23 17:31:15 -0600

    feat: allows onClick handler to focus search area

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 // @ts-nocheck
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import { CharactersContext } from "../context/Context";
 import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
 
@@ -9,9 +9,11 @@ const NavBar = () => {
   const [nav, setNav] = useState(false);
   const [color, setColor] = useState("transparent");
 
+  const ref = useRef(null);
+
   const handleNav = () => {
     setNav(!nav);
-    console.log('object');
+    ref.current.focus();
   };
 
   useEffect(() => {
@@ -26,7 +28,10 @@ const NavBar = () => {
   }, []);
 
   return (
-    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300 shadow-outline" style={{background: `${color}`}}>
+    <div
+      className="fixed left-0 top-0 w-full z-10 ease-in duration-300 shadow-outline"
+      style={{ background: `${color}` }}
+    >
       <div className="max-w-[1240px] m-auto flex justify-between items-center p-3">
         <h1 className="mr-auto text-3xl">Rick & Morty</h1>
         <div className="hidden sm:flex ml-auto">
@@ -55,6 +60,7 @@ const NavBar = () => {
             <input
               type="text"
               placeholder="Search..."
+              ref={ref}
               className="flex-initial py-1 pl-4 rounded-2xl focus:outline-none focus-visible:outline-offset-1 focus-visible:outline-1 focus-visible:outline-black max-w-[250px]"
               onChange={(event) => {
                 makeQuery(event.target.value);
@@ -62,7 +68,10 @@ const NavBar = () => {
               value={query}
             />
           </div>
-          <div className="mx-auto text-gray-700 flex-none pl-3" onClick={handleNav}>
+          <div
+            className="mx-auto text-gray-700 flex-none pl-3"
+            onClick={handleNav}
+          >
             <AiOutlineClose size={25} />
           </div>
         </div>
